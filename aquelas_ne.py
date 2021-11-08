d42322ab912a40f3c6644e90021ae86b2148703f
Bizotto <bizotto.nicolas@gmail.com>
2021-11-08 18:13:35 -0300

    inicio da jornada pt2

---
+++ aquelas_ne.py
@@ -0,0 +1,23 @@
+from abc import get_cache_token
+
+
+print('a')
+print('ola mundo')
+
+iten = True or False
+type
+
+
+if iten:
+    {True:
+
+     print('meu pau eh gigante ')
+     }
+else:
+    {False:
+     print('pau pequeno')
+
+     }
+
+
+# meu primeiro python esta sendo produzido
