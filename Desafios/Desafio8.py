2a60d6eeee18a772b62edf7c963ee20da5b833be
Bizotto <bizotto.nicolas@gmail.com>
2022-05-17 18:25:46 -0300

    did something

--- Desafios/Desafio8.py
+++ Desafios/Desafio8.py
@@ -2,12 +2,13 @@ while True:
   op=int(input('type 1 to enter the system n 0 to leave: '))
   if op == 1:
     totalPaint= 2
-    wallHeight = float(input('Type the wall height: '))
-    wallWidth = float(input('type wall width: ')) 
+    wallHeight = float(input('\nType the wall height: '))
+    wallWidth = float(input('\nType wall width: ')) 
     wallArea = wallHeight*wallWidth
     canPaint= wallArea/totalPaint
 
-  print('you can paint a total of {}'.format(canPaint))
+  print('\nyour wall is {}x{}, your area is {}'.format(wallHeight,wallWidth,wallArea))
+  print('\nyou can paint a total of {}'.format(canPaint))
 
   if op == 0:
     break
